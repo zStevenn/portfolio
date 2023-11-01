c5c41df92fd7d5727d16e19ad9a78afc6bbf913a
Steven <steventje678@hotmail.com>
2023-11-01 21:49:06 +0100

    Implement darkMode local storage

--- src/hooks/context/useThemeContext.tsx
+++ src/hooks/context/useThemeContext.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { createContext, useContext, useState, Dispatch, SetStateAction } from "react"
+import { createContext, useContext, useState, Dispatch, SetStateAction, useEffect, useLayoutEffect } from "react"
 
 export const ThemeContext = createContext<ThemeContext | null>(null)
 
@@ -17,6 +17,21 @@ type ThemeContext = {
 
 export default function ThemeContextProvider({ children }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>('dark')
+
+  useLayoutEffect(() => {
+    if (typeof Window !== "undefined" && window.localStorage) {
+      const storedTheme = localStorage.getItem("darkMode")
+      console.log(storedTheme)
+      if (storedTheme) setTheme(storedTheme as Theme)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (typeof Window !== "undefined" && window.localStorage) {
+      const storedTheme = localStorage.getItem("darkMode")
+      if (storedTheme === null || storedTheme !== theme) localStorage.setItem("darkMode", theme)
+    }
+  }, [theme])
 
   return (
     <ThemeContext.Provider
