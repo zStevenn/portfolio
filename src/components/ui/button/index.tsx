7ed374bc7e516f665198a2242797757944bec7fa
Steven <steventje678@hotmail.com>
2023-11-01 21:25:20 +0100

    styles buttons

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -8,7 +8,7 @@ type ButtonProps = {
 function Button({ onClick, className, children }: ButtonProps) {
   return (
     <button
-      className='border px-2 py-1'
+      className={`rounded hover:scale-95 transition-all border border-neutral-950 hover:bg-red-700/70 hover:border-red-700 hover:shadow hover:shadow-red-700 hover:text-neutral-200 px-2 py-1 dark:border-neutral-700 dark:hover:border-red-700 ${className ? className : null}`}
       onClick={onClick}
     >
       {children}
